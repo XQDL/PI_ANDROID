fa0b355c59c2a78302959e1f739bd17823134df7
Andre Vieira <andrevinnicius89@gmail.com>
2022-03-15 21:24:10 -0300

    Add telas de historico coordenador e alunos, e tela inicial do coordenador

--- PI/DaHora/app/src/main/java/com/pi/dahora/MainActivity.kt
+++ PI/DaHora/app/src/main/java/com/pi/dahora/MainActivity.kt
@@ -2,10 +2,17 @@ package com.pi.dahora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.login_btn).setOnClickListener{ x() }
     }
+    private fun x(){
+        setContentView(R.layout.activity_requirement_history_studant)
+    }
+
+
 }
