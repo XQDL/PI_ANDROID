63a2a0fdc79586d747ae64f179d5b3a930a26d3d
Andre Vieira <andrevinnicius89@gmail.com>
2022-03-29 20:32:48 -0300

    ATT DATABASE ORM

---
+++ PI/DaHora/app/src/main/java/com/pi/dahora/RequirementHistoryStudent.kt
@@ -0,0 +1,75 @@
+package com.pi.dahora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.room.Room
+import com.pi.dahora.database.AppDatabase
+import com.pi.dahora.model.User
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var _users: List<User>
+    private lateinit var _db: AppDatabase
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        _db = getConnection()
+
+        //_db.userDao().insertAll(User(1, "Andre", "123"),
+          //  User(2, "Adailson", "123"))
+
+
+
+
+
+        findViewById<Button>(R.id.login_btn).setOnClickListener{ x() }
+    }
+
+    private fun attHistory(){
+        try{
+            val userDao = _db.userDao()
+            _users = userDao.getAll()
+            findViewById<TextView>(R.id.name).text = _users[0].name
+            findViewById<TextView>(R.id.password).text = _users[0].password
+
+        }catch (e : Exception){
+            println("Unexpected error: $e")
+        }
+    }
+
+
+    private fun x(){
+        setContentView(R.layout.activity_requirement_history_studant)
+        findViewById<Button>(R.id.att).setOnClickListener{_ -> run { attHistory() } }
+
+        try{
+            val userDao = _db.userDao()
+            _users = userDao.getAll()
+
+
+
+            findViewById<TextView>(R.id.name).text = _users[0].name
+            findViewById<TextView>(R.id.password).text = _users[0].password
+
+        }catch (e : Exception){
+            println("Unexpected error: $e")
+        }
+
+    }
+
+
+    private fun getConnection(): AppDatabase {
+        return Room.databaseBuilder(
+            applicationContext,
+            AppDatabase::class.java, "database-name"
+        ).allowMainThreadQueries().fallbackToDestructiveMigration().build()
+    }
+
+
+
+}
