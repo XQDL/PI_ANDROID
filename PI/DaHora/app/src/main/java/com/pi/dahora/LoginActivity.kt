b8efb2613a1a8590444a762647d4fe0bbd558e60
adailson4s <adailson.up@gmail.com>
2022-04-28 14:42:11 -0300

    Chamada de APi nao funcionando

--- PI/DaHora/app/src/main/java/com/pi/dahora/LoginActivity.kt
+++ PI/DaHora/app/src/main/java/com/pi/dahora/LoginActivity.kt
@@ -2,8 +2,15 @@ package com.pi.dahora
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.pi.dahora.Models.Cordinator
+import com.pi.dahora.Models.Endpoint
+import com.pi.dahora.Utils.NetworkUtils
 import com.pi.dahora.databinding.ActivityLoginBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -13,6 +20,7 @@ class LoginActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         initView()
+        getData()
     }
 
     fun initView(){
@@ -22,5 +30,27 @@ class LoginActivity : AppCompatActivity() {
                 startActivity(this)
             }
         }
+    }
+
+    fun getData() {
+        val retrofitClient = NetworkUtils
+            .getRetrofitInstance("https://localhost:5060")
+
+        val endpoint = retrofitClient.create(Endpoint::class.java)
+        val callback = endpoint.getPosts()
+
+        callback.enqueue(object : Callback<List<Cordinator>> {
+            override fun onFailure(call: Call<List<Cordinator>>, t: Throwable) {
+                Toast.makeText(baseContext, t.message, Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onResponse(call: Call<List<Cordinator>>, response: Response<List<Cordinator>>) {
+                response.body()?.forEach {
+                    binding.name.hint= it.name
+                    //textView.text = textView.text.toString().plus(it.body)
+                }
+            }
+        })
+
     }
 }
---
+++ PI/DaHora/app/src/main/java/com/pi/dahora/Models/Cordinator.kt
@@ -0,0 +1,24 @@
+package com.pi.dahora.Models
+
+import com.google.gson.annotations.SerializedName
+import retrofit2.Call
+import retrofit2.http.GET
+
+data class Cordinator(
+    @SerializedName("id")
+    var id : Int,
+    @SerializedName("name")
+    var name : String,
+    @SerializedName("email")
+    var email : String,
+    @SerializedName("password")
+    var password : String,
+    @SerializedName("phoneNumber")
+    var phoneNumber : String
+    )
+
+interface Endpoint {
+
+    @GET("api/coordinator")
+    fun getPosts() : Call<List<Cordinator>>
+}
